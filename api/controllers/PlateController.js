05352d8246070b65cb824e881a43b40cbd8c467e
Fer Dingler <ferdingler@gmail.com>
2014-11-17 15:56:06 -0800

    Plate simulation

--- api/controllers/PlateController.js
+++ api/controllers/PlateController.js
@@ -9,6 +9,21 @@ module.exports = {
 
 	view: function(req, res){
 		res.view('plate');
+	},
+
+	dummyPlate: function(req, res){
+
+		var data = [];
+		data.push(['ID', '', '', '', '']);
+		var c = 1;
+    	for(var i=1; i<12; i+=2){
+			for(var j=1; j<12; j+=2){
+	    		data.push([c + '', i, j, 'Not annotated', 1]);
+	    		c++;
+	    	}    		
+    	}
+
+    	return res.send(data);
 	}
 	
 };
