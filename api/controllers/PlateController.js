67127b96aec43c0eea9e9a48033b46ea3516caee
Fer Dingler <ferdingler@gmail.com>
2014-11-17 13:45:19 -0800

    Plate view

---
+++ api/controllers/PlateController.js
@@ -0,0 +1,15 @@
+/**
+ * PlateController
+ *
+ * @description :: Server-side logic for managing plates
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+
+	view: function(req, res){
+		res.view('plate');
+	}
+	
+};
+
